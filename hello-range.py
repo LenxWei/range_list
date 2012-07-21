272d81bf45f87d1d5e70583583ccae5a04f3dcfa
Lenx Tao Wei <lenx.wei@gmail.com>
2012-07-21 02:25:49 -0700

    add ut

--- hello-range.py
+++ hello-range.py
@@ -3,9 +3,18 @@
 #  or copy at http://www.boost.org/LICENSE_1_0.txt)
 #  Hello World Example from the tutorial
 
-import range_list
+from range_list import *
 
-a=range_list.range_item(2,3)
-print a
-a.size=4
-print a
+a=range_item(2,3)
+assert a!=None
+l=range_list()
+l.insert(a)
+assert len(l)==1
+b1=l.search(2)
+assert a==b1
+b2=l.search(10)
+assert b2==None
+
+for i in l:
+	print i
+	
