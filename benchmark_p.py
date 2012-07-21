da9791daae4c1915ffd29cf552a9ec5bd69d102f
Lenx Tao Wei <lenx.wei@gmail.com>
2012-07-21 02:54:16 -0700

    add config.py

--- benchmark_p.py
+++ benchmark_p.py
@@ -1,9 +1,10 @@
 from range_list0 import *
 from random import randrange, seed
 import time
+from config import *
 
 seed(time.time())
-s=100000
+
 l=range_list()
 for i in xrange(1,s):
 	a=range_item(randrange(0x7fffffff),randrange(0xff))
