365c38a028e3ed2e1f5f2066c33ee8c00ef4a643
Lenx Tao Wei <lenx.wei@gmail.com>
2012-07-21 03:08:38 -0700

    add search benchmark

---
+++ benchmark_cs.py
@@ -0,0 +1,14 @@
+from range_list import *
+from random import randrange, seed
+import time
+from config import *
+
+seed(time.time())
+
+l=range_list()
+for i in xrange(1,s):
+	a=range_item(randrange(0x7fffffff),randrange(0xff))
+	l.insert(a)
+	
+for i in xrange(1,s):
+        l.search(randrange(0x7fffffff))
