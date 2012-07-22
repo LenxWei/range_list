129b60017e2a32b01d2a67b9a378272f393f8f75
Lenx Tao Wei <lenx.wei@gmail.com>
2012-07-22 11:50:57 -0700

    use object directly in range_list

--- hello-range.py
+++ hello-range.py
@@ -1,6 +1,14 @@
 from range_list import *
 from random import randrange
 
+class range_item:
+    # "range" is reserved
+    def __init__(self,address,size):
+        self.address = address
+        self.size = size
+    def __str__(self):
+        return "%x:%x"%(self.address,self.size)
+
 a=range_item(2,3)
 assert a!=None
 l=range_list()
--- range_list.cpp
+++ range_list.cpp
@@ -15,53 +15,10 @@ using namespace boost::python;
 
 typedef unsigned long addr_t;
 
-struct range_item{
-	const addr_t address;
-	addr_t size;
-    object obj;
-
-	range_item():address(0),size(0)
-	{}
-
-	range_item(addr_t a, addr_t s):address(a),size(s)
-	{}
-
-	range_item(addr_t a, addr_t s, object x):address(a),size(s),obj(x)
-	{}
-
-	bool has(addr_t a)const
-	{
-		return address <= a && a < address+size;
-	}
-
-	bool operator==(const range_item& o)const
-	{
-		return address==o.address && size==o.size;
-	}
-
-};
-
-range_item r_None;
-
-ostream& operator<<(ostream& o, const range_item& r)
-{
-	ostringstream s;
-	s << hex << r.address <<":"<< r.size ;
-	o << s.str();
-	return o;
-}
-
-#if 1
-typedef map<addr_t, range_item, std::less<addr_t>,
-			boost::fast_pool_allocator<pair<const addr_t, range_item>
-				//,boost::default_user_allocator_new_delete
-				//,boost::details::pool::null_mutex
+typedef map<addr_t, object, std::less<addr_t>,
+			boost::fast_pool_allocator<pair<const addr_t, object>
 			>
 		> range_map_t;
-#else
-typedef map<addr_t, range_item
-> range_map_t;
-#endif
 
 typedef range_map_t::iterator range_iter;
 
@@ -79,26 +36,6 @@ range_iter operator-(range_iter x, unsigned long c)
     return x;
 }
 
-bool operator==(const range_item& s, const object& o)
-{
-	if(o.is_none())
-		return s.address==0;
-	{
-		extract<range_item&> x(o);
-		if(x.check()){
-			range_item& y=x();
-			return s.address==y.address && s.size==y.size;
-		}
-	}
-
-	return false;
-}
-
-bool operator!=(const range_item& s, const object& o)
-{
-	return !(s==o);
-}
-
 struct range_slice{
     range_iter _begin;
     range_iter _end;
@@ -111,12 +48,12 @@ struct range_slice{
         return *this;
     }
 
-    range_item& next()
+    object next()
     {
         if(_begin==_end)
             boost::python::objects::stop_iteration_error();
         else{
-            range_item& k=_begin->second;
+            object& k=_begin->second;
             ++_begin;
             return k;
         }
@@ -125,6 +62,12 @@ struct range_slice{
 
 struct range_list{
 	range_map_t _data;
+
+    bool __has(const range_iter& it, addr_t a) // it!=end(), a >= it->first
+    {
+        size_t s=extract<size_t>(it->second.attr("size"));
+        return it->first+s > a;
+    }
 
 	size_t size()const
 	{
@@ -144,17 +87,26 @@ struct range_list{
 		return _data.end();
 	}
 
-	bool insert(const range_item& obj)
+	bool insert(const object& obj)
 	{
-		range_iter it_l=_simple_search(obj.address), end=_data.end();
-		range_iter it_h=_simple_search(obj.address+obj.size-1);
-		if(it_l!=it_h)
-			return false;
-		if(it_l!=end){
-			if(it_l->second.has(obj.address))
-				return false; // intersection
+	    addr_t address=extract<addr_t>(obj.attr("address"));
+	    size_t size=extract<size_t>(obj.attr("size"));
+		range_iter it_l=_simple_search(address), end=_data.end();
+		if(it_l==end){
+		    if( address+size <= begin()->first)
+                goto ok;
+		    return false; //intersect
 		}
-		_data.insert(make_pair(obj.address, obj));
+		if(__has(it_l, address)) // intersect
+            return false;
+
+		range_iter it_h=it_l; ++it_h;
+		if(it_h==end)
+			goto ok;
+		if(address+size > it_h->first)
+            return false;
+    ok:
+		_data.insert(make_pair(address, obj));
 		return true;
 	}
 
@@ -180,9 +132,11 @@ struct range_list{
 	void remove_address(addr_t address)
 	{
 		range_iter it=_simple_search(address);
-		if(it!=end() && it->second.has(address)){
-			_data.erase(it);
-			return;
+		if(it!=end()){
+            if(__has(it,address)){
+                _data.erase(it);
+                return;
+            }
 		}
 		throw std::invalid_argument("not in list");
 	}
@@ -191,16 +145,16 @@ struct range_list{
 	range_iter index(addr_t address)
 	{
 		range_iter it=_simple_search(address), end=_data.end();
-		if(it==end || !it->second.has(address))
+		if(it==end || !__has(it,address))
 			throw std::invalid_argument("not in list");
 		return it;
 	}
 
-	range_item& search(addr_t address)
+	object search(addr_t address)
 	{
 		range_iter it = _simple_search(address), end=_data.end();
-		if(it==end || !it->second.has(address))
-			return r_None;
+		if(it==end || !__has(it,address))
+			return object();
 		return it->second;
 	}
 
@@ -209,18 +163,12 @@ struct range_list{
 		range_iter it=_simple_search(address), end=_data.end();
 		if(it==end)
 			return make_tuple(object(), begin());
-		if(it->second.has(address))
+		if(__has(it,address))
 			return make_tuple(it,it);
-		range_iter next=it;
-                ++next;
+		range_iter next=it; ++next;
 		if(next==end)
 			return make_tuple(it,object());
 		return make_tuple(it, next);
-	}
-
-	range_item& finger(addr_t address)
-	{
-		return search(address);
 	}
 
 	range_iter begin()
@@ -248,7 +196,7 @@ struct range_list{
         return range_slice(begin(),end());
     }
 
-	range_item& at(const range_iter& it)
+	object at(const range_iter& it)
 	{
 		if(it==_data.end())
 			throw invalid_argument("null iter");
@@ -263,24 +211,8 @@ struct range_list{
     }
 };
 
-void translator(const invalid_argument& x) {
-	PyErr_SetString(PyExc_UserWarning, x.what());
-}
-
 BOOST_PYTHON_MODULE(range_list)
 {
-	// register_exception_translator<
-	// invalid_argument>(translator);
-	class_<range_item>("range_item", init<addr_t, addr_t>())
-        .def(init<addr_t, addr_t, object>())
-	.def_readonly("address", &range_item::address)
-	.def_readwrite("size", &range_item::size)
-	.def_readwrite("obj", &range_item::obj)
-	.def("has", &range_item::has)
-	.def(self_ns::str(self_ns::self))
-	.def(self_ns::operator==(self_ns::self, object()))
-	.def(self_ns::operator!=(self_ns::self, object()))
-	;
 	class_<range_list>("range_list")
 	.def("__iter__", &range_list::all)
 	.def("__len__", &range_list::size)
@@ -290,14 +222,14 @@ BOOST_PYTHON_MODULE(range_list)
 	.def("remove_address", &range_list::remove_address)
 	.def("delete", &range_list::del)
 	.def("merge", &range_list::merge)
-	.def("search", &range_list::search,return_internal_reference<>())
+	.def("search", &range_list::search)
 	.def("index",&range_list::index)
 	.def("begin",&range_list::begin)
 	.def("end",&range_list::end)
-	.def("at", &range_list::at,return_internal_reference<>())
+	.def("at", &range_list::at)
 	.def("slice",&range_list::slice)
 	.def("slice",&range_list::slice2)
-	.def("finger", &range_list::finger,return_internal_reference<>())
+	.def("finger", &range_list::search)
 	.def("detailed_search",&range_list::detailed_search)
 	;
 	class_<range_iter>("range_iter")
@@ -308,7 +240,7 @@ BOOST_PYTHON_MODULE(range_list)
 	;
 	class_<range_slice>("range_slice", init<range_iter, range_iter>())
 	.def("__iter__", &range_slice::own, return_internal_reference<>())
-	.def("next", &range_slice::next, return_internal_reference<>())
+	.def("next", &range_slice::next)
 	;
 }
 
